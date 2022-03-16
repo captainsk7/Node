35785cbff187e47278c0ad6f1fc59cf9761d53c5
captainsk7 <shaileshkondke7@gmail.com>
2022-03-17 02:50:40 +0530

    My changes

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const express = require("express");
 const app = express();
 
 app.get("/", function (req, res) {
-  res.send("Hello World,IT WORKED!");
+  res.send("Hello World,\nIT WORKED !");
 });
 app.get("/new", function (req, res) {
-  res.send("New also worked");
+  res.send("NEW ALSO WORKED !");
 });
 app.listen(process.env.PORT || 5000);
 module.exports = app;
