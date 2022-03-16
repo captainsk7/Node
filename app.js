8d9169814f827f57c26d90012534b0328359d876
captainsk7 <shaileshkondke7@gmail.com>
2022-03-17 02:44:36 +0530

    new changes

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const app = express();
 app.get("/", function (req, res) {
   res.send("Hello World,IT WORKED!");
 });
-
+app.get("/new", function (req, res) {
+  res.send("New also worked");
+});
 app.listen(process.env.PORT || 5000);
 module.exports = app;
